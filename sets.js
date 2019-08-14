feba827653a1e1bc3072768a5c9aad622a060176
prashant333 <pmani157@gmail.com>
2019-08-14 12:04:45 +0530

    set functions added

--- sets.js
+++ sets.js
@@ -18,14 +18,63 @@ function mySet() {
     this.add = function(element){
         if(!this.has(element)){
             index = collection.push(element);
+            return 'added successfully';
+        }
+        return 'Item already present';
+    };
+
+    // this method is to remove an elements from set
+    this.remove = function (element) {
+        if (this.has(element)) {
+            index = collection.indexOf(element);
+            collection.splice(index ,1);
             return true;
         }
         return false;
-    }
+    };
+
+    // returns the size of set
+    this.size = function(){
+        return collection.length;
+    };
+
+    // this method returns union of two set
+    this.union = function(otherSet) {
+        let unionSet = new mySet();
+        let firstSet = this.value();
+        let secondSet = otherSet.value();
+        firstSet.forEach(function(e){
+            unionSet.add(e);
+        });
+        secondSet.forEach(function(e){
+            unionSet.add(e);
+        });
+        return unionSet;
+    };
+
+    // this method returns intersection of two set
+    this.intersection = function(otherSet) {
+        let intersectionSet = new mySet();
+        let firstSet = this.values();
+        firstSet.forEach(function(e){
+            if(otherSet.has(e)){
+                intersectionSet.add(e);
+            }
+        });
+        return intersectionSet;
+    };
 }
 
 let setVar = new mySet();
+let setVar1 = new mySet();
 
+setVar1.add('are');
+setVar1.add('you');
 setVar.add('hello');
 setVar.add('how');
+console.log(setVar.union(setVar1))
 console.log(setVar.has('hello'))
+setVar.remove('hello')
+console.log(setVar.has('hello'))
+console.log(setVar.value());
+console.log(setVar.add('how'));
