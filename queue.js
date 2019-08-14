ae0757e343758b41c2d58eb321afa581751da1a5
prashant333 <pmani157@gmail.com>
2019-08-14 13:13:22 +0530

    priority queue

--- queue.js
+++ queue.js
@@ -32,3 +32,51 @@ function Queue(){
  queueOne.enqueue('world');
  queueOne.dequeue();
  queueOne.print()
+
+//  priority queue.
+function PriorityQueue () {
+    var collection = [];
+    this.printCollection = function() {
+      (console.log(collection));
+    };
+    this.enqueue = function(element){
+        if (this.isEmpty()){ 
+            collection.push(element);
+        } else {
+            var added = false;
+            for (var i=0; i<collection.length; i++){
+                 if (element[1] < collection[i][1]){ //checking priorities
+                    collection.splice(i,0,element);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added){
+                collection.push(element);
+            }
+        }
+    };
+    this.dequeue = function() {
+        var value = collection.shift();
+        return value[0];
+    };
+    this.front = function() {
+        return collection[0];
+    };
+    this.size = function() {
+        return collection.length; 
+    };
+    this.isEmpty = function() {
+        return (collection.length === 0); 
+    };
+}
+
+var pq = new PriorityQueue(); 
+pq.enqueue(['prashant', 2]); 
+pq.enqueue(['Raj', 3]);
+pq.enqueue(['Ram', 1])
+pq.enqueue(['jhon', 2])
+pq.printCollection();
+pq.dequeue();
+console.log(pq.front());
+pq.printCollection();
